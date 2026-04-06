f2cadfdc2e327b385c6ba797a372af0e511aa93f
LuisG <luisguillermoa8@gmail.com>
2026-04-05 21:37:12 -0500

    Initial commit

---
+++ src/main/java/pe/edu/pe/smartrent_backend/SmartRentBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.smartrent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartRentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pe/smartrent_backend/SmartRentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.smartrent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
