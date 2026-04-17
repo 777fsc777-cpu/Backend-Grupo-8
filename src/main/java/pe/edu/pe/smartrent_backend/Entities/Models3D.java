04a01350caf9927f208635372f665f4488176d80
ruthantonnn28 <ruthantonnn24@gmail.com>
2026-04-17 03:30:21 -0500

    Tabla: Models3D.java, RiskPoints.java, Estate.java

---
+++ src/main/java/pe/edu/pe/smartrent_backend/Entities/Models3D.java
@@ -0,0 +1,79 @@
+package pe.edu.pe.smartrent_backend.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Models3D")
+public class Models3D {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idModels3D;
+
+    @Column(name = "fileURL", length = 200, nullable = false)
+    private String fileURL;
+
+    @Column(name = "state", length = 100, nullable = false)
+    private String state;
+
+    @Column(name = "createDate", nullable = false)
+    private LocalDate createDate;
+
+    @OneToOne
+    @JoinColumn(name = "idProperty")
+    private Estate estate;
+
+    public Models3D() {
+    }
+
+    public Models3D(Integer idModels3D, String fileURL, String state, LocalDate createDate, Estate estate) {
+        this.idModels3D = idModels3D;
+        this.fileURL = fileURL;
+        this.state = state;
+        this.createDate = createDate;
+        this.estate = estate;
+    }
+
+    public Integer getIdModels3D() {
+        return idModels3D;
+    }
+
+    public void setIdModels3D(Integer idModels3D) {
+        this.idModels3D = idModels3D;
+    }
+
+    public String getFileURL() {
+        return fileURL;
+    }
+
+    public void setFileURL(String fileURL) {
+        this.fileURL = fileURL;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public Estate getEstate() {
+        return estate;
+    }
+
+    public void setEstate(Estate estate) {
+        this.estate = estate;
+    }
+}
+
