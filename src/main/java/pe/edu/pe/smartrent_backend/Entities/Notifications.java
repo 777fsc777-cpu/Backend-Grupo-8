6294e22c61fde0f46573a0f2a680e17c186ee2b7
LuisG <luisguillermoa8@gmail.com>
2026-04-17 02:43:56 -0500

    Roles y Usuarios

---
+++ src/main/java/pe/edu/pe/smartrent_backend/Entities/Notifications.java
@@ -0,0 +1,101 @@
+package pe.edu.pe.smartrent_backend.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Notification")
+public class Notifications {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idNotification;
+
+    @Column(name = "title", length = 50, nullable = false)
+    private String title;
+
+    @Column(name = "message", length = 800, nullable = false)
+    private String message;
+
+    @Column(name = "type", length = 50, nullable = false)
+    private String type;
+
+    @Column(name = "read", nullable = false)
+    private Boolean read;
+
+    @Column(name = "createdDate", nullable = false)
+    private LocalDate createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "idUser")
+    private Users user;
+
+    public Notifications() {}
+
+    public Notifications(Integer idNotification, String title, String message, String type, Boolean read, LocalDate createdDate, Users user) {
+        this.idNotification = idNotification;
+        this.title = title;
+        this.message = message;
+        this.type = type;
+        this.read = read;
+        this.createdDate = createdDate;
+        this.user = user;
+    }
+
+    public Integer getIdNotification() {
+        return idNotification;
+    }
+
+    public void setIdNotification(Integer idNotification) {
+        this.idNotification = idNotification;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getRead() {
+        return read;
+    }
+
+    public void setRead(Boolean read) {
+        this.read = read;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/pe/edu/pe/smartrent_backend/Entities/UsersBackground.java
@@ -0,0 +1,92 @@
+package pe.edu.pe.smartrent_backend.Entities;
+
+
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class UsersBackground {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idBackground;
+
+    @Column(name = "type", length = 50, nullable = false)
+    private String type;
+
+    @Column(name = "description", length = 200, nullable = false)
+    private String description;
+
+    @Column(name = "source", length = 50, nullable = false)
+    private String source;
+
+    @Column(name = "registrationDate", nullable = false)
+    private LocalDate registrationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "idUser")
+    private Users user;
+
+    public UsersBackground() {
+    }
+
+    public UsersBackground(Integer idBackground, String type, String description, String source, LocalDate registrationDate, Users user) {
+        this.idBackground = idBackground;
+        this.type = type;
+        this.description = description;
+        this.source = source;
+        this.registrationDate = registrationDate;
+        this.user = user;
+    }
+
+    public Integer getIdBackground() {
+        return idBackground;
+    }
+
+    public void setIdBackground(Integer idBackground) {
+        this.idBackground = idBackground;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+}
