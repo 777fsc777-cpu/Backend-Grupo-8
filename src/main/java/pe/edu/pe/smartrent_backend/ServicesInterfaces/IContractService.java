0437a986ef3ebae7d49d6607c3252402e0dff08d
777fsc777-cpu <777fsc777@gmail.com>
2026-04-20 21:10:15 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/pe/smartrent_backend/ServicesInterfaces/IContractService.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.api9233.servicesinterfaces;
+
+import pe.edu.upc.api9233.entities.Contract;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IContractService {
+    List<Contract> list();
+    Contract insert(Contract c);
+    Optional<Contract> listId(int id);
+    void update(Contract c);
+    void delete(int id);
+}
