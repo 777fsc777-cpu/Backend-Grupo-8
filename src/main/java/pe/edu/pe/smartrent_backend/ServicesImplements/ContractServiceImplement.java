054fb3b400f7491580e67cafc768db00daca93f1
777fsc777-cpu <777fsc777@gmail.com>
2026-04-27 11:22:36 -0500

    Add methods to retrieve income and estates without contracts

--- src/main/java/pe/edu/pe/smartrent_backend/ServicesImplements/ContractServiceImplement.java
+++ src/main/java/pe/edu/pe/smartrent_backend/ServicesImplements/ContractServiceImplement.java
@@ -1,11 +1,16 @@
 package pe.edu.pe.smartrent_backend.ServicesImplements;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pe.edu.pe.smartrent_backend.DTOS.contractDTOS.EstateWithoutActiveContractDTO;
+import pe.edu.pe.smartrent_backend.DTOS.contractDTOS.LessorIncomeDTO;
 import pe.edu.pe.smartrent_backend.Entities.Contract;
 import pe.edu.pe.smartrent_backend.Repositories.IContractRepository;
 import pe.edu.pe.smartrent_backend.ServicesInterfaces.IContractService;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ContractServiceImplement implements IContractService {
@@ -33,9 +38,30 @@ public class ContractServiceImplement implements IContractService {
         cR.save(c);
     }
 
-    
     @Override
     public void delete(int id) {
         cR.deleteById(id);
+    }
+
+    @Override
+    public List<LessorIncomeDTO> getIncomeByLessor() {
+        return cR.getIncomeByLessor().stream().map(row ->
+                new LessorIncomeDTO(
+                        ((Number) row[0]).intValue(),
+                        (String) row[1],
+                        ((Number) row[2]).longValue(),
+                        ((Number) row[3]).doubleValue()
+                )
+        ).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<EstateWithoutActiveContractDTO> getEstatesWithoutActiveContract() {
+        return cR.getEstatesWithoutActiveContract().stream().map(row ->
+                new EstateWithoutActiveContractDTO(
+                        ((Number) row[0]).intValue(),
+                        (String) row[1]
+                )
+        ).collect(Collectors.toList());
     }
 }
